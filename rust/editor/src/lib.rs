d19806b47dc5b1d84c04178ab5986e26ee839b92
Yixin <yixinbobbywu@outlook.com>
2023-12-01 16:52:12 +0800

    feat: use struct to pack and non-blocking read

--- rust/editor/src/lib.rs
+++ rust/editor/src/lib.rs
@@ -7,25 +7,39 @@ use crossterm::{
 use std::{
     error::Error,
     io::{self},
+    time::Duration,
 };
-use crossterm::event::KeyEventKind::Repeat;
 
-pub fn run() -> Result<(), Box<dyn Error>> {
-    init_terminal()?;
+pub struct Editor {
+    should_quit: bool,
+}
+impl Editor {
+    pub fn new() -> Self {
+        Self { should_quit: false }
+    }
 
-    loop {
-        let event = read()?;
-        if let Event::Key(key_event) = event {
-            println!("{:?}\r", event);
-            if let KeyCode::Char(c) = key_event.code {
-                println!("{}\r", c);
-                if c == 'q' {
-                    break;
+    pub fn run(&mut self) -> Result<(), Box<dyn Error>> {
+        init_terminal()?;
+
+        loop {
+            if crossterm::event::poll(Duration::from_millis(100))? {
+                let event = read()?;
+                if let Event::Key(key_event) = event {
+                    println!("{:?}\r", event);
+                    if let KeyCode::Char(c) = key_event.code {
+                        println!("{}\r", c);
+                        if c == 'q' {
+                            self.should_quit = true;
+                            break;
+                        }
+                    }
                 }
+            } else {
+                println!("100ms no input\r");
             }
         }
+        Ok(())
     }
-    Ok(())
 }
 
 fn init_terminal() -> Result<(), Box<dyn Error>> {
--- rust/editor/src/main.rs
+++ rust/editor/src/main.rs
@@ -1,9 +1,8 @@
-use editor::run;
+use editor::Editor;
 
 fn main() {
-    if let Err(e) = run(){
-        println!("{}",e);
+    if let Err(e) = Editor::new().run() {
+        println!("{}", e);
         std::process::exit(1);
     }
 }
-
